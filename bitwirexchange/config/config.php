29e39158662da2aa43b5f4fd16353f6f0ca8c0b4
pv16061995 <pv16061995@gmail.com>
2017-12-13 13:55:41 +0530

    Extra folder

--- bitwirexchange/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('PROJECT_TITLE','Bitwire');
-define('BASE_PATH','http://localhost/bitwirexchange/bitwirexchange/');
-
-?>
--- bitwirexchange/testapi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-print_r(json_encode($movies));
-
-
-?>
